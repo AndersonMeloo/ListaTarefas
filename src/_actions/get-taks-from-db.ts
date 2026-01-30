3e0a4fa1135bbc1a79c420aff7914438ec0792f3
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-01-29 23:11:28 -0300

    feat: export const getTasks

--- src/_actions/get-taks-from-db.ts
+++ src/_actions/get-taks-from-db.ts
@@ -1,6 +1,8 @@
+'use server'
+
 import { prisma } from "../utils/prisma"
 
-const getTasks = async () => {
+export const getTasks = async () => {
 
     const tasks = await prisma.task.findMany();
 
