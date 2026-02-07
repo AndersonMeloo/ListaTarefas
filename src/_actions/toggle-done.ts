6c8ac013eb220182b6053d1dbd755931bdda9b74
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-02-06 23:31:24 -0300

    chore: create toggle-done.ts

---
+++ src/_actions/toggle-done.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { prisma } from "../utils/prisma";
+
+export const updateTaskStatus = async (taskId: string) => {
+
+    try {
+        const currentTask = await prisma.task.findUnique({
+
+            where: { id: taskId }
+        })
+
+        if (!currentTask) return
+
+        const updatedStatus = await prisma.task.update({
+            where: { id: taskId },
+            data: { done: !currentTask.done }
+        })
+
+        if (!updatedStatus) return
+
+        return updatedStatus
+    } catch (error) {
+        throw error
+    }
+}
