ed7c57ce6dc60bbfde0389415bb3eecc5b41f372
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-02-07 01:39:00 -0300

    chore: create EdiTaskProps editTask connection DB in Prisma

--- src/_actions/edit-task.ts
+++ src/_actions/edit-task.ts
@@ -1,3 +1,25 @@
 'use server'
-
 import { prisma } from "../utils/prisma"
+
+type EditTaskProps = {
+    idTask: string
+    newTask: string
+}
+
+export const editTask = async ({ idTask, newTask }: EditTaskProps) => {
+
+    try {
+
+        if (!idTask || !newTask) return
+
+        const editedTask = await prisma.task.update({
+            where: { id: idTask },
+            data: { task: newTask }
+        })
+
+        if (!editedTask) return
+
+    } catch (error) {
+        throw error
+    }
+}
